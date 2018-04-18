45b7a9ca89e0279efb4019d2f97a34a7c4c8bcb5
Rijul1999 <rijul.rakhi@gmail.com>
2018-04-19 02:14:13 +0530

    Added the train_encoder function

--- train.py
+++ train.py
@@ -1,6 +1,9 @@
 import pickle
 
-from .utils import generate_cloned_samples
+import torch
+from torch.Autograd import Variable
+
+from utils import generate_cloned_samples
 
 
 def get_cloned_voices(no_speakers,no_cloned_texts):
@@ -9,7 +12,7 @@ def get_cloned_voices(no_speakers,no_cloned_texts):
             cloned_voices = pickle.load(fp)
     except:
         cloned_voices = generate_cloned_samples()
-    if(numpy.array(cloned_voices).shape != (no_speakers , no_cloned_texts))
+    if(numpy.array(cloned_voices).shape != (no_speakers , no_cloned_texts)):
         cloned_voices = generate_cloned_samples("./Cloning_Audio/cloning_text.txt" ,no_speakers,True,0)
 
     return cloned_voices
@@ -22,3 +25,83 @@ def get_speaker_embeddings(model):
     embed = model.embed_speakers.weight.data
     shape = embed.shape
     return embed,shape
+
+
+def train_encoder(encoder, speakers, embeddings, batch_size=[1,1], epochs=1000):
+	
+	criterion = nn.L1Loss()
+	optimizer = torch.optim.SGD(encoder.parameters(),lr=0.002)
+
+	for i in range(epochs):
+		
+		for j in range(batch_size[0]):
+			
+			for k in range(batch_size[1]):
+				
+				elem = speakers[j][k]
+				elem = np.reshape(elem, (1,1,elem.shape[0],elem.shape[1]))				
+				
+				if(k==0):
+					inner_inputs = elem
+				else:
+					inner_inputs = np.hstack((inner_inputs,elem))
+			
+			if(j==0):
+				true_inputs = inner_inputs
+				embed = embeddings[i]
+			else:
+				true_inputs = np.vstack((true_inputs,inner_inputs))
+				embed = np.vstack((embed,embeddings[i]))
+		
+		
+		optimizer.zero_grad()
+		input_to_encoder = Variable(torch.from_numpy(true_inputs).type(torch.FloatTensor))
+		output_from_encoder = encoder(input_to_encoder)
+
+		embeddings = Variable(torch.from_numpy(embed).type(torch.LongTensor))
+
+		loss = criterion(output_from_encoder,embeddings)
+		loss.backward()
+		optimizer.step()
+		save_checkpoint({
+            	'epoch': epoch + 1,
+            	'arch': args.arch,
+            	'state_dict': model.state_dict(),
+            	'best_prec1': best_prec1,
+            	'optimizer' : optimizer.state_dict(),
+        	})
+
+
+def save_checkpoint(state,filename = "model_checkpoint.pth.tar"):
+	torch.save(state, filename)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+					
+		
+		
+	
+	
