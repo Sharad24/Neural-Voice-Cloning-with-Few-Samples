7a3f21658125af1a12363ab88d2682b79951d043
Sharad24 <sharadchitlangia24sc@gmail.com>
2018-04-19 02:32:46 +0530

    Removed Unnecessary code

--- Modules/Cloning_Samples_Attention.py
+++ Modules/Cloning_Samples_Attention.py
@@ -34,48 +34,30 @@ class MultiHeadAttention(nn.Module):
         K = F.elu(self.key_layer(keys))
         V = F.elu(self.value_layer(keys))
 
-        # split each Q, K and V into h different values from dim 2
-        # and then merge them back together in dim 0
         chunk_size = int(self._num_units / self._h)
         Q = torch.cat(Q.split(split_size=chunk_size, dim=2), dim=0)
         K = torch.cat(K.split(split_size=chunk_size, dim=2), dim=0)
         V = torch.cat(V.split(split_size=chunk_size, dim=2), dim=0)
 
-        # calculate QK^T
         attention = torch.matmul(Q, K.transpose(1, 2))
-        # normalize with sqrt(dk)
         attention = attention / torch.sqrt(self._key_dim)
-        # use masking (usually for decoder) to prevent leftward
-        # information flow and retains auto-regressive property
-        # as said in the paper
+
         if self._is_masked:
             diag_vals = attention[0].sign().abs()
             diag_mat = diag_vals.tril()
             diag_mat = diag_mat.unsqueeze(0).expand(attention.size())
-            # we need to enforce converting mask to Variable, since
-            # in pytorch we can't do operation between Tensor and
-            # Variable
+
             mask = Variable(
                 torch.ones(diag_mat.size()) * (-2**32 + 1), requires_grad=False)
-            # this is some trick that I use to combine the lower diagonal
-            # matrix and its masking. (diag_mat-1).abs() will reverse the value
-            # inside diag_mat, from 0 to 1 and 1 to zero. with this
-            # we don't need loop operation and could perform our calculation
-            # faster
+
             attention = (attention * diag_mat) + (mask * (diag_mat-1).abs())
-        # put it to softmax
         attention = F.softmax(attention, dim=-1)
-        # apply dropout
         attention = F.dropout(attention, self._dropout_p)
-        # multiplyt it with V
         attention = torch.matmul(attention, V)
-        # convert attention back to its input original size
         restore_chunk_size = int(attention.size(0) / self._h)
         attention = torch.cat(
             attention.split(split_size=restore_chunk_size, dim=0), dim=2)
-        # residual connection
         attention += query
-        # apply batch normalization
         attention = self.bn(attention.transpose(1, 2)).transpose(1, 2)
 
         return attention
