cb6bf42511280f9efe19f283d2450f74778e3112
Sharad Chitlangia <sharadchitlangia@Sharads-MacBook-Pro.local>
2018-06-29 01:51:16 +0530

    Added function for dataset

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import torch
 import numpy as np
 import librosa
 import librosa.display
+from torch.utils.data import Dataset, Dataloader
 
 # need this for English text processing frontend
 import nltk
@@ -75,3 +76,37 @@ def generate_cloned_samples(model,cloning_text_path  = None, no_speakers = 108 ,
     # all speakers[speaker_id][cloned_audio_number]
     # print(all_speakers[0][1].shape)
     return all_speakers
+
+class Speech_Dataset(Dataset):
+    def __init__(self, mfccs, embeddings):
+        '''Mfccs have to be list of lists of numpy arrays. Each of these numpy arrays will be a mel spectrogram'''
+        self.voices = mfccs
+        largest_size = self.voice[0][0]
+        temp = [spec.shape[1] for text in self.voices for spec in text]
+        largest_size = np.amax(np.array(temp))
+        self.voices = _pad(self.voices, largest size)
+        self.embeddings = embeddings
+
+    def _pad(specs, maximum_size):
+        '''Input:
+        Specs: Mel Spectrograms with 80 channels but the length of each channel is not the same.
+        maximum_size: Largest channel length. Others are padded to this length
+
+        Padding with 0 won't affect the convolutions because anyway the neurons corresponding to the states have to
+        be dead if they are not padded. Putting 0 will also make those neurons dead. And later an average is taken along
+        this dimension too.
+
+        Returns: A padded array of arrays of spectrograms.'''
+        for i in specs:
+            for j in i:
+                final = np.zeros((80, maximum_size))
+                final[:, :specs[i][j].shape[1]] = specs[i][j]
+                specs[i][j]=final
+        return final
+
+    def __len__(self):
+        '''Returns total number of speakers'''
+        return  len(self.voices)
+
+    def __getitem__(self, idx):
+        return (self.voices[idx], self.embeddings[idx])
